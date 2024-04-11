bd922266c399c0d93fea31444e7926a74f3c1035
Abraham J. Reines <118085620+reinesaj2@users.noreply.github.com>
2024-04-10 21:02:07 -0700

    Wed Apr 10 21:01:49 PDT 2024

--- CS559/Project6/CheckingLimits.py
+++ CS559/Project6/CheckingLimits.py
@@ -1,7 +1,7 @@
 """
 Author: Abraham Reines
 Created: Mon Apr  1 12:55:40 PDT 2024
-Modified: Mon Apr  8 15:15:03 PDT 2024
+Modified: Wed Apr 10 14:19:06 PDT 2024
 Name of file: CheckingLimits.py
 """
 
@@ -9,6 +9,7 @@ from sympy import symbols, limit, oo
 
 n = symbols('n')
 f = 10**12 * n**3 + 10**6 * n**2 + n + 1
+g = n**4
 
 def AsymptoteTime(f, g, n):
     """
@@ -26,17 +27,17 @@ def AsymptoteTime(f, g, n):
     # Calculate the limit for g(n)/f(n) as n approaches infinity
     limit_g_over_f = limit(g/f, n, oo)
 
-    # Big O condition: f(n) is O(g(n)) if limit of f(n)/g(n) is finite and not zero
-    analysis_results['Big O'] = limit_f_over_g.is_finite and limit_f_over_g != 0
+    # Big O condition: f(n) is O(g(n)) if limit of f(n)/g(n) is finite
+    analysis_results['Big O'] = limit_f_over_g.is_finite
 
     # small o condition: f(n) is o(g(n)) if limit of f(n)/g(n) is zero
     analysis_results['Small o'] = limit_f_over_g == 0
 
-    # Omega condition: f(n) is Ω(g(n)) if limit of g(n)/f(n) is zero
-    analysis_results['Omega'] = limit_g_over_f == 0
+    # Omega condition: f(n) is Ω(g(n)) if limit of f(n)/g(n) is infinite
+    analysis_results['Omega'] = limit_f_over_g == oo
 
     return analysis_results
 
 # g(n) is n**4 for comparison
-results = AsymptoteTime(f, n**4, n)
+results = AsymptoteTime(f, g, n)
 print(results)
